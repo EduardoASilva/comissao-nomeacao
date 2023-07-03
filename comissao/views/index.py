43195d154dd73e96346b37bf9cf9aaf8cf02dbf5
EduardoASilva <eduardoaugustosilva87@gmail.com>
2023-07-03 11:25:28 -0300

    Atualização na função para mostrar os membros na div cargo e no index.html no checkbox

--- comissao/views/index.py
+++ comissao/views/index.py
@@ -2,15 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
-from comissao.models import ListaCargos
+from comissao.models import ListaCargos, Membros
 
 
 @login_required(login_url='login')
 def index(request):
     if request.user.is_staff:
         lista_cargos = ListaCargos.objects.all()
+        membros = Membros.objects.all()
         context = {
-            'cargos': lista_cargos
+            'cargos': lista_cargos,
+            'membros': membros
         }
         return render(request, 'index.html', context)
     else:
