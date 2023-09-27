ce76fda1cb8830a56eb9fe879d69d8ebc5cbb12e
eduardo <eduardoaugustosilva87@gmail.com>
2023-09-26 22:46:19 -0300

    ALterando as funções JS

--- comissao/admin.py
+++ comissao/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from comissao.models import ListaCargos, Membros
+
+
+class AdminListaCargos(admin.ModelAdmin):
+    list_display = ('nome',)
+    list_per_page = 15
+
+    class AdminMembros(admin.ModelAdmin):
+        list_display = ('nome',)
+        list_per_page = 15
+
+
+admin.site.register(ListaCargos)
+admin.site.register(Membros)
--- comissao/static/js/main.js
+++ comissao/static/js/main.js
@@ -1,65 +1,105 @@
- //inicio função envio do cargo para div cargos//
+//  //inicio função envio do cargo para div cargos//
+//
+// const btn = document.querySelector('#confirmar_cargo');
+//
+//       confirmar_cargo.addEventListener("click" ,function(e) {
+//
+//         e.preventDefault();
+//
+//         const check = document.querySelector(".listCargos");
+//
+//         const value = check.value;
+//
+//         // let checkbox = document.getElementById('cargo');
+//         if (check.checked) {
+//             document.getElementById("nome_cargo").innerHTML = value;
+//         } else {
+//             alertWarning("Selecione um Cargo");
+//         }
+//         });
+//
+//  //fim função envio do cargo para div cargos//
+//
+//  //Inicio função desabilitar checks//
+//
+// (function (){
+//     "use strict";
+//
+//     let marcado = 0;
+//     let verificarcheck = function ($checks) {
+//         if (marcado > 0 ) {
+//             loop($checks, function($element){
+//                 $element.disabled = $element.checked ? '' : 'disabled';
+//             });
+//         } else {
+//             loop($checks, function($element){
+//                 $element.disabled = '';
+//             });
+//         }
+//     };
+//
+//     let loop = function($element, cb){
+//         let max = $element.length;
+//         while(max--) {
+//             cb($element[max]);
+//         }
+//     }
+//     let count = function($element){
+//         return $element.checked ? marcado + 1 : marcado - 1;
+//     }
+//
+//     window.onload = function(){
+//         let $checks = document.querySelectorAll('.listCargos');
+//         loop($checks, function($element){
+//             $element.onclick = function(){
+//                 marcado = count(this);
+//                 verificarcheck($checks);
+//             }
+//             if($element.checked) marcado = marcado + 1;
+//         });
+//
+//         verificarcheck($checks);
+//     }
+// }());
+//
+// //Fim função desabilitar checks//
 
-const btn = document.querySelector('#confirmar_cargo');
+// window.onload = function(){
+//     let checkbox = document.querySelectorAll(".listCargos");
+//     loop(checkbox, function (el){
+//
+//     })
+// }
+//
+//
+// loop()
+//
+// checkbox.addEventListener('click', function (){
+//     if (this.checked){
+//         alert('check')
+//     }else{
+//         alert('no check')
+//     }
+// })
 
-      confirmar_cargo.addEventListener("click" ,function(e) {
+function checkbox(el){
+    let checkbox = document.querySelectorAll(".listCargos");
 
-        e.preventDefault();
-
-        const check = document.querySelector(".listCargos");
-
-        const value = check.value;
-
-        // let checkbox = document.getElementById('cargo');
-        if (check.checked) {
-            document.getElementById("nome_cargo").innerHTML = value;
-        } else {
-            alertWarning("Selecione um Cargo");
-        }
-        });
-
- //fim função envio do cargo para div cargos//
-
- //Inicio função desabilitar checks//
-
-(function (){
-    "use strict";
-
-    let marcado = 0;
-    let verificarcheck = function ($checks) {
-        if (marcado > 0 ) {
-            loop($checks, function($element){
-                $element.disabled = $element.checked ? '' : 'disabled';
-            });
-        } else {
-            loop($checks, function($element){
-                $element.disabled = '';
-            });
-        }
-    };
-
-    let loop = function($element, cb){
-        let max = $element.length;
-        while(max--) {
-            cb($element[max]);
-        }
-    }
-    let count = function($element){
-        return $element.checked ? marcado + 1 : marcado - 1;
-    }
-
-    window.onload = function(){
-        let $checks = document.querySelectorAll('.listCargos');
-        loop($checks, function($element){
-            $element.onclick = function(){
-                marcado = count(this);
-                verificarcheck($checks);
+    if (el.checked){
+        for (const check of checkbox){
+            if (!check.checked){
+                check.disabled = true
             }
-            if($element.checked) marcado = marcado + 1;
-        });
-
-        verificarcheck($checks);
+        }
+    }else{
+        for (const check of checkbox){
+            check.disabled = false
+        }
     }
-}());
 
-//Fim função desabilitar checks//
+    // if (el.checked){
+    //     alert('check')
+    // }else{
+    //     alert('no check')
+    // }
+}
